ae27f14f34c72d5682948673eac0fa1bcdb3dd61
Souad Malak Lamrizegar <souad.lamrizegar@univ-constantine2.dz>
2025-11-26 10:39:56 +0100

    Fix urls.py and add API home endpoint

--- campus_management/urls.py
+++ campus_management/urls.py
@@ -1,29 +1,20 @@
-"""
-URL configuration for campus_management project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+# Page d'accueil API
+def home(request):
+    return JsonResponse({
+        "status": "API is running",
+        "service": "Courses API",
+        "endpoints": {
+            "courses": "/api/courses/courses/",
+            "enrollments": "/api/courses/enrollments/"
+        }
+    })
 
 urlpatterns = [
+    path('', home),  # Page home
     path('admin/', admin.site.urls),
-    path('api/courses/', include('courses.urls')),  # ← AJOUTE CETTE LIGNE
+    path('api/courses/', include('courses.urls')),
 ]
