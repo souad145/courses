3c24f8237abeb141138636fdcbb73ff27900b80b
Souad Malak Lamrizegar <souad.lamrizegar@univ-constantine2.dz>
2025-11-26 10:31:32 +0100

    Add REST_FRAMEWORK JSON renderer for production

--- campus_management/settings.py
+++ campus_management/settings.py
@@ -119,3 +119,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key type
 # ----------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
